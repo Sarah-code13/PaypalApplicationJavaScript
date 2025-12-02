09282c6af5246e52a037301cf2dbbab7e77ed44e
Sarah Perdomo <sarahdanielap@gmail.com>
2025-12-01 19:45:56 -0500

    fixing cors issue

--- project-spa-backend/app.js
+++ project-spa-backend/app.js
@@ -16,11 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
